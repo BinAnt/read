453109b7a8799648d1a0c484bbe90f69bd1fd22c
BinAnt <binbuyanza@gmail.com>
2020-09-27 18:28:37 +0800

    函数的扩产

--- ECMAScript-6入门/code/01.js
+++ ECMAScript-6入门/code/01.js
@@ -1,9 +1,14 @@
-const cat = {
-  lives: 9,
-  jumps: () => {
-    this.lives--;
-  },
-};
+// function factorial(n) {
+//   if (n === 1) return 1;
+//   return n * factorial(n - 1);
+// }
 
-cat.jumps();
-console.log(cat.lives);
+// console.log(factorial(5));
+
+// 尾调用
+function factorial(n, total) {
+  if (n === 1) return total;
+  return factorial(n - 1, n * total);
+}
+
+console.log(factorial(5, 1)); // 120
